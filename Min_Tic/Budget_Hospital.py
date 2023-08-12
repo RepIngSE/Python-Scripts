44d59a3be7509acf90ad6956a91524424dc6d9bc
Sgabi2604 <139818962+Sgabi2604@users.noreply.github.com>
2023-08-11 23:29:41 -0500

    Create

---
+++ Min_Tic/Budget_Hospital.py
@@ -0,0 +1,8 @@
+Ejercicio presupuesto en un hospital
+presupuesto = float(input("Ingrese el valor del presupuesto: "))
+ginecologia = presupuesto*0.4
+traumatologia = presupuesto*0.3
+pediatria = presupuesto*0.3
+print("El presupuesto para ginecología es: ",ginecologia, "mcte.")
+print("Para traumatología es: ",traumatologia, "mcte.")
+print("Para pediatría es: ",pediatria, "mcte.")
