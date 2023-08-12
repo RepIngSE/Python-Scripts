775b9af90958b6a4a2f1c6b8e0f435224c338429
Sgabi2604 <139818962+Sgabi2604@users.noreply.github.com>
2023-08-11 23:23:54 -0500

    Create

---
+++ Min_Tic/Exercise_Percent.py
@@ -0,0 +1,8 @@
+# Porcentaje de hombres y mujeres.
+nh = int(input("Ingrese el número de hombres: "))
+nm = int(input("Ingrese el número de mujeres: "))
+ta = nh + nm
+ph = (nh/ta)*100
+pm = (nm/ta)*100
+print("El porcentaje de hombres es: ", ph,"%")
+print("El porcentaje de mujeres es: ", pm,"%")
