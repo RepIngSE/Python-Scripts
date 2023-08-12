391920fa6338fcd0c019aa01d1eefed563f007aa
Sgabi2604 <139818962+Sgabi2604@users.noreply.github.com>
2023-08-11 23:27:20 -0500

    Create Dough_Formula.py

---
+++ Min_Tic/Dough_Formula.py
@@ -0,0 +1,8 @@
+//Calcular la masa, la cual consiste en masa = presión * volumen / a.37 * (temperatura+460)
+presion = float(input("Ingrese el valor de la presión: "))
+volumen = float(input("Ingrese el volumen: "))
+temperatura = float(input("Ingrese la temperatura: "))
+
+masa = (presion*volumen)/(0.37*(temperatura+460))
+
+print("El valor de la masa de aire es: ",round(masa,2))
